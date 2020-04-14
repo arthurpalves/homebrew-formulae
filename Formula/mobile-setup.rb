2486fc435cbe6306f9650d97a47f99e091ff116b
Arthur Alves <arthura@backbase.com>
2020-04-14 11:56:58 +0200

    hellow world

---
+++ Formula/mobile-setup.rb
@@ -0,0 +1,27 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class MobileSetup < Formula
+  desc ""
+  homepage "https://github.com/arthurpalves/mobile-setup.git"
+  head "https://github.com/arthurpalves/mobile-setup.git"
+
+  # depends_on "cmake" => :build
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test mobile-setup`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
