74074bb0590a1e2e543f9512b8146bec48dee1e2
Arthur Alves <arthuralves.p@gmail.com>
2020-05-20 17:52:55 +0200

    coherent-swift release 0.4.1

--- Formula/coherent-swift.rb
+++ Formula/coherent-swift.rb
@@ -2,8 +2,8 @@
 class CoherentSwift < Formula
   desc "A command-line tool that measures cohesion in your Swift codebase"
   homepage "https://github.com/arthurpalves/coherent-swift.git"
-  url "https://github.com/arthurpalves/coherent-swift.git", tag: "0.4.0"
-  version "0.4.0"
+  url "https://github.com/arthurpalves/coherent-swift.git", tag: "0.4.1"
+  version "0.4.1"
 
   depends_on :macos
   depends_on :xcode
