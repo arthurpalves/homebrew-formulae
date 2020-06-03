dcdd538509afcc1e189a7271f49daa3b7995b413
Arthur Alves <arthura@backbase.com>
2020-06-03 10:10:50 +0200

    badgy release 0.1.3 - resolves #1

--- Formula/badgy.rb
+++ Formula/badgy.rb
@@ -2,8 +2,8 @@
 class Badgy < Formula
   desc "Badgy is a command line tool that creates variants of your icon by adding badge overlays."
   homepage "https://github.com/arthurpalves/badgy.git"
-  url "https://github.com/arthurpalves/badgy.git", tag: "0.1.2"
-  version "0.1.2"
+  url "https://github.com/arthurpalves/badgy.git", tag: "0.1.3"
+  version "0.1.3"
 
   depends_on :macos
   depends_on :xcode
